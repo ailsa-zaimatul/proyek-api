42fb477b143ce8a1fbbbf38f581d5aebf81984bf
ailsa-zaimatul <fitriailsa29@gmail.com>
2026-06-03 11:55:27 +0700

    db.config.js

--- config/db.config.js
+++ config/db.config.js
@@ -1,6 +1,6 @@
 import { Sequelize} from "sequelize";
 import mysql12 from "mysql2";
-const db = new Sequelize ('web_lanjut', 'avnadmin', 'AVNS_dMyS0WcG3jJ-6uWAWCN',{
+const db = new Sequelize ('defaultdb', 'avnadmin', 'AVNS_dMyS0WcG3jJ-6uWAWCN',{
     host: "mysql-b09e3ec-fitriailsa29-18cc.h.aivencloud.com",
     dialect: "mysql",
     port:18218,
