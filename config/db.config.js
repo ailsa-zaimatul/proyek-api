84889014dcaa421e76a34df030d14e236602dbb5
ailsa-zaimatul <fitriailsa29@gmail.com>
2026-06-03 11:50:38 +0700

    Update db.config.js

--- config/db.config.js
+++ config/db.config.js
@@ -1,6 +1,6 @@
 import { Sequelize} from "sequelize";
 import mysql12 from "mysql2";
-const db = new Sequelize ('web_lanjut', 'root', '',{
+const db = new Sequelize ('web_lanjut', 'mysql-b09e3ec-fitriailsa29-18cc.h.aivencloud.com', '',{
     host: "localhost",
     dialect: "mysql",
     "define": {
